d4ef7c97aef80eb2d5535b45cbb6267b8fd17361
rukevweubio22 <ubiorukevwe@gmail.com>
2024-10-02 14:53:04 +0000

    second commit

---
+++ models/staging/createtable.sql
@@ -0,0 +1,10 @@
+{{config(materialized='table')}}
+select 'ali' as name1
+union all
+select 'alim' as name2 
+union all
+select 'ruky' as name3 
+union all
+select 'ubio' as name4 
+union all
+select 'efe' as name5 
